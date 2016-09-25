682e3aa4ec9e1c1ab641bbe4ba00fda12eb2ca84
Victor Lee <myleevictor@gmail.com>
2016-09-25 16:57:58 +0800

    fixing optional bug when transition to checkOutVC

--- Shopping Cart/CheckoutViewController.swift
+++ Shopping Cart/CheckoutViewController.swift
@@ -7,29 +7,32 @@
 
 import UIKit
 
+
 class CheckoutViewController: UIViewController {
 
-    @IBOutlet weak var totalBillLabel: UILabel!
+    
+    @IBOutlet weak var finalBillLabel: UILabel!
     @IBOutlet weak var standardShippingButton: UIButton!
     @IBOutlet weak var expressShippingButton: UIButton!
     
+    //MARK: Properties
     var shoppingCart: ShoppingCart?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationItem.title = "Check out"
+        self.shoppingCart = ShoppingCart()
         standardShippingButton.isEnabled = false    // default is shown as standard shipping selected
         self.updateUI()
     }
     
+    
     func updateUI() {
         
-//        totalBillLabel.text = "test"
         
-        if let shoppingCart = shoppingCart {
-        
-            totalBillLabel.text = "\(shoppingCart.getTotal())"
+        if let totalAmount = shoppingCart?.getTotal() {
+            self.finalBillLabel.text = "Total \(totalAmount)"
         }
     }
     
@@ -61,28 +64,26 @@ class CheckoutViewController: UIViewController {
         self.updateUI()
     }
     
+    
     @IBAction func placeOrderDidTap(_ sender: AnyObject)
     {
-        if let shoppingCart = shoppingCart {
-            shoppingCart.getTotal()
-            self.updateUI()
-        }
-        
-        let alertController = UIAlertController(title: "Confirm Order", message: "Please confirm that you want to make a payment of \(shoppingCart!.getTotal())!", preferredStyle: .alert)
+       
+
+        let alertController = UIAlertController(title: "Confirm Order", message: "Please confirm that you want to make a payment of \(shoppingCart?.getTotal())!", preferredStyle: .alert)
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
-            let successActionSheet = UIAlertController(title: "Cancel", message: "Your order of \(self.shoppingCart!.getTotal()) will be cancelled! Please shop again!", preferredStyle: .actionSheet)
+            let successActionSheet = UIAlertController(title: "Cancel", message: "Your order of \(self.shoppingCart?.getTotal()) will be cancelled! Please shop again!", preferredStyle: .actionSheet)
             let cancelPlaceOrderAction = UIAlertAction(title: "Let's Shop More!", style: .default, handler: { (action) in
-            self.shoppingCart!.reset()
+            self.shoppingCart?.reset()
             })
             successActionSheet.addAction(cancelPlaceOrderAction)
             self.dismiss(animated: true, completion: nil)
             self.present(successActionSheet, animated: true, completion: nil)
         }
         let confirmAction = UIAlertAction(title: "Confirm", style: .default) { (action) in
-            let successActionSheet = UIAlertController(title: "Thank you!", message: "Your payment of \(self.shoppingCart!.getTotal()) was processed successfully! Please check your email for your order receipt email and shipping information.", preferredStyle: .actionSheet)
+            let successActionSheet = UIAlertController(title: "Thank you!", message: "Your payment of \(self.shoppingCart?.getTotal()) was processed successfully! Please check your email for your order receipt email and shipping information.", preferredStyle: .actionSheet)
             let continueShoppingAction = UIAlertAction(title: "Let's Shop More!", style: .default, handler: { (action) in
                 // TODO: - reset shopping cart, pop to root VC
-                self.shoppingCart!.reset()
+                self.shoppingCart?.reset()
                 _ = self.navigationController?.popToRootViewController(animated: true)
             })
             
@@ -95,4 +96,5 @@ class CheckoutViewController: UIViewController {
         alertController.addAction(confirmAction)
         self.present(alertController, animated: true, completion: nil)
     }
+
 }
